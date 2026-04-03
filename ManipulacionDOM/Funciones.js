85535509ed793465d4d42de78755a16b73e8fb99
Davinson <Davinsonchz@gmail.com>
2026-04-03 15:56:39 -0500

    Resolviendo

--- ManipulacionDOM/Funciones.js
+++ ManipulacionDOM/Funciones.js
@@ -8,3 +8,26 @@ function calcularIva(){
     document.getElementById("resultadoTotal").innerHTML=  "Precio Final: "+precioFinal
 
 }
+
+function calcularCuadrado(){
+    let lado = parseFloat(document.getElementById("cuadrado").value)
+
+    let Area= lado * lado
+    let Perimetro = lado * 4
+
+    document.getElementById("resultadoArea").innerHTML = "El area es: " + Area
+    document.getElementById("resultadoPerimetro").innerHTML = "El Perimetro es: " + Perimetro
+
+}
+
+function numeroDosDigitos(){
+    let number = parseInt(document.getElementById("Digito").value)
+
+    let decenas = Math.floor(number/10)
+    let unidades = number % 10
+
+    document.getElementById("resDecenas").innerHTML = "Decenas: " + decenas
+    document.getElementById("resUnidades").innerHTML = "Unidades: " + unidades
+
+
+}
